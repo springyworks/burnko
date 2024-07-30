0628e4d082b5cc5137b66d6b1052ebe6aa02bce2
springyworks <kostoel@gmail.com>
2024-07-30 13:46:45 +0200

    jjj

--- examples/show-image-of/src/bin/test.rs
+++ examples/show-image-of/src/bin/test.rs
@@ -1,17 +1,24 @@
-//rust
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+// Import burn packages
+use burn::prelude::*;
+use burn_ndarray::NdArray;
+use burn::backend::Wgpu;
+
+// Type alias for the backend to use.
+//type Backend = Wgpu;
+
+// Type alias for the backend
+type Backend = NdArray<f32>; 
 
 fn main() {
-    let x = 10;
-    let y = 20;
+    println!("Hello, world!");
+    let device=Default::default();
+  
+    // Creation of two tensors, the first with explicit values and the second one with ones, with the same shape as the first
+    let tensor_1 = Tensor::<Backend, 2>::from_data([[2., 3.], [4., 5.]], &device);
+    let tensor_2 = Tensor::<Backend, 2>::ones_like(&tensor_1);
 
-    println!("{x}{y}");
-    println!("Image buffer created successfully! x = {}, y = {}", x, y);
-    let number: f64 = 1.0;
-    let width: usize = 5;
-    println!("{number:>width$}");
-
-    let f = 255.555555;
-    let dec = 3;
-    let width = 10;
-    println!("{f} to {dec} decimal places is {f:-^width$.dec$} very cool!");
-}
+    // Print the element-wise addition (done with the WGPU backend) of the two tensors.
+    println!("{}", tensor_1 + tensor_2);}
